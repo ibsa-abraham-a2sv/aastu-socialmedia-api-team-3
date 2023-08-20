2883f0a11b8a5632e6add1eb6ee5ee79893a7578
Abdulsemed-Abdelila <abdulsemed.abdelila@a2sv.org>
2023-08-20 15:08:53 +0300

    add unit test

--- src/Galacticos.Infrastructure/Data/ApiDbContext.cs
+++ src/Galacticos.Infrastructure/Data/ApiDbContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Galacticos.Domain.Entities;
+
 namespace Galacticos.Infrastructure.Data
 {
     public class ApiDbContext : DbContext
