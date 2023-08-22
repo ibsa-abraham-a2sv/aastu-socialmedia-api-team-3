769cc2dc168275105442466c42a144ddb0357a6a
Abdulsemed-Abdelila <abdulsemed.abdelila@a2sv.org>
2023-08-22 22:41:25 +0300

    fix relations repo

--- src/Galacticos.Application/Features/Relation/Request/Query/GetFollowedUsersRequest.cs
+++
@@ -1,9 +0,0 @@
-using MediatR;
-
-namespace Galacticos.Application.Features.Relation.Request.Query
-{
-    public class GetFollowedUsersRequest : IRequest<List<Guid>>
-    {
-        public Guid Id { get; set; }
-    }
-}
