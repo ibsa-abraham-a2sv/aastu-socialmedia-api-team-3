f82fe08636ef66d0b270475b59fcb29ff2d293ab
yonibabi <yonatan.tesfaye@a2sv.org>
2023-08-23 16:58:27 +0300

    Post Func Done

---
+++ src/Galacticos.Application/DTOs/Posts/CreatePostRequestDTO.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Galacticos.Application.DTOs.Posts
+{
+    public class CreatePostRequestDTO
+    {
+        public string Caption { get; set; } = null!;
+        public string Image { get; set; } = "";
+         
+    }
+}
